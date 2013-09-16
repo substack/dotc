77821563030f5471f30653240275e0e003321ad4
James Halliday <mail@substack.net>
2013-09-16 20:15:46 +0100

    using gcc -print-search-dirs to get the gcc cc1 path

--- example/main.c
+++ example/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#require "./foo.c" as foo
+#require "./foo.c" as foo
 
 int main(int argc, char **argv) {
-    //printf("%d\n", foo(atoi(argv[1])));
+    printf("%d\n", foo(atoi(argv[1])));
     return 0;
 }
