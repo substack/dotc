5d60702f20bdab314cc390f5a56b7b94c7114225
James Halliday <mail@substack.net>
2013-09-18 19:54:10 -0700

    turn the loader into the main

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 var fs = require('fs');
 var path = require('path');
 var through = require('through');
-var parse = require('./parse.js');
+var parse = require('./lib/parse.js');
 
 module.exports = function (files) {
+    if (!Array.isArray(files)) files = [ files ];
+    
     var output = through();
     files = files.slice();
     
     (function next () {
         if (files.length === 0) return output.queue(null);
         var file = files.shift();
-        var rs = fs.createReadStream(file);
+        var rs = isStream(file) ? file : fs.createReadStream(file);
         var p = rs.pipe(parse(path.dirname(file)));
         
         p.on('data', function (buf) { output.queue(buf) });
@@ -19,3 +21,7 @@ module.exports = function (files) {
     
     return output;
 };
+
+function isStream (s) {
+    return s && typeof s.pipe === 'function';
+}
